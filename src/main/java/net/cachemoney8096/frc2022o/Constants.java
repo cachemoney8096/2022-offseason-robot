cbddf4abea156f9dbcfec30c2437fd44e7626f04
Basel Alghanem <basel@umich.edu>
2022-09-23 20:13:56 -0500

    Add functions to get the drivetrain to move

--- src/main/java/net/cachemoney8096/frc2022o/Constants.java
+++ src/main/java/net/cachemoney8096/frc2022o/Constants.java
@@ -1,9 +1,15 @@
 package net.cachemoney8096.frc2022o;
+import edu.wpi.first.math.util.Units;
 
 // Store physical / mathematical constants here
 public class Constants {
+  // Swerve module encoder offsets (in radians!!)
   public static final double FRONT_LEFT_STEER_OFFSET_RAD = 2.0;
   public static final double FRONT_RIGHT_STEER_OFFSET_RAD = 2.0;
   public static final double BACK_LEFT_STEER_OFFSET_RAD = 2.0;
   public static final double BACK_RIGHT_STEER_OFFSET_RAD = 2.0;
+
+  // Drivetrain size
+  public static final double TRACK_WIDTH_METERS = Units.inchesToMeters(23.5);
+  public static final double WHEEL_BASE_METERS = Units.inchesToMeters(23.5);
 }
---
+++ src/main/java/net/cachemoney8096/frc2022o/FeatureFlags.java
@@ -0,0 +1,6 @@
+package net.cachemoney8096.frc2022o;
+
+// Use this class to enable / disable certain special features
+public class FeatureFlags {
+  public static final boolean ENABLE_HEADING_LOCK_TELEOP = true;
+}
