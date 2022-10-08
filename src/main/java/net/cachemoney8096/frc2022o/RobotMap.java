f70bb92f62065e2464b66e68eacd38fbdace1a6e
Basel Alghanem <basel@umich.edu>
2022-10-07 19:43:16 -0500

    Fixes some more comments to be multi-line style

--- src/main/java/net/cachemoney8096/frc2022o/RobotMap.java
+++ src/main/java/net/cachemoney8096/frc2022o/RobotMap.java
@@ -1,54 +1,54 @@
 package net.cachemoney8096.frc2022o;
 
-// Store CAN and port IDs here
+/** Store CAN and port IDs here */
 public class RobotMap {
-  // Driver/Operator controller indices
-  public static final int DRIVER_CONTROLLER_INDEX = 0;
-  public static final int OPERATOR_CONTROLLER_INDEX = 1;
+  /** Driver/Operator controller indices */
+  public static final int DRIVER_CONTROLLER_INDEX = 0,
+      OPERATOR_CONTROLLER_INDEX = 1;
 
-  // Drivetrain CAN IDs
-  public static final int DRIVE_FRONT_LEFT_ID = 1;
-  public static final int STEER_FRONT_LEFT_ID = 2;
-  public static final int DRIVE_FRONT_RIGHT_ID = 3;
-  public static final int STEER_FRONT_RIGHT_ID = 4;
-  public static final int DRIVE_BACK_LEFT_ID = 5;
-  public static final int STEER_BACK_LEFT_ID = 6;
-  public static final int DRIVE_BACK_RIGHT_ID = 7;
-  public static final int STEER_BACK_RIGHT_ID = 8;
+  /** Drivetrain CAN IDs */
+  public static final int DRIVE_FRONT_LEFT_ID = 1,
+      STEER_FRONT_LEFT_ID = 2,
+      DRIVE_FRONT_RIGHT_ID = 3,
+      STEER_FRONT_RIGHT_ID = 4,
+      DRIVE_BACK_LEFT_ID = 5,
+      STEER_BACK_LEFT_ID = 6,
+      DRIVE_BACK_RIGHT_ID = 7,
+      STEER_BACK_RIGHT_ID = 8;
 
-  // Intake CAN IDs
-  public static final int INTAKE_MOTOR_ONE_ID = 9;
-  public static final int INTAKE_MOTOR_TWO_ID = 10;
-  public static final int INTAKE_MOTOR_THREE_ID = 11;
+  /** Intake CAN IDs */
+  public static final int INTAKE_MOTOR_ONE_ID = 9,
+      INTAKE_MOTOR_TWO_ID = 10,
+      INTAKE_MOTOR_THREE_ID = 11;
 
-  // Indexer CAN IDs
-  public static final int INDEXER_MOTOR_ONE_ID = 12;
-  public static final int INDEXER_MOTOR_TWO_ID = 13;
-  public static final int INDEXER_MOTOR_THREE_ID = 14;
+  /** Indexer CAN IDs */
+  public static final int INDEXER_MOTOR_ONE_ID = 12,
+      INDEXER_MOTOR_TWO_ID = 13,
+      INDEXER_MOTOR_THREE_ID = 14;
 
-  // Shooter CAN IDs
-  public static final int SHOOTER_MOTOR_ONE_ID = 15;
-  public static final int SHOOTER_MOTOR_TWO_ID = 16;
-  public static final int HOOD_MOTOR_ID = 17;
+  /** Shooter CAN IDs */
+  public static final int SHOOTER_MOTOR_ONE_ID = 15,
+      SHOOTER_MOTOR_TWO_ID = 16,
+      HOOD_MOTOR_ID = 17;
 
-  // Climber CAN IDs
-  public static final int CLIMBER_MOTOR_RIGHT_ID = 18;
-  public static final int CLIMBER_MOTOR_LEFT_ID = 19;
+  /** Climber CAN IDs */
+  public static final int CLIMBER_MOTOR_RIGHT_ID = 18,
+      CLIMBER_MOTOR_LEFT_ID = 19;
 
-  // CAN IDs for things other than motor controllers
+  /** CAN IDs for things other than motor controllers */
   public static final int PIGEON_IMU_ID = 20;
 
-  // Sensor DIO
-  public static final int SWERVE_FRONT_LEFT_DIO = 0;
-  public static final int SWERVE_FRONT_RIGHT_DIO = 1;
-  public static final int SWERVE_BACK_LEFT_DIO = 2;
-  public static final int SWERVE_BACK_RIGHT_DIO = 3;
-  public static final int INDEXER_CARGO_DIO = 4;
-  public static final int INTAKE_CARGO_DIO = 5;
-  public static final int HOOD_ENCODER_DIO = 6;
+  /** Sensor DIO */
+  public static final int SWERVE_FRONT_LEFT_DIO = 0,
+      SWERVE_FRONT_RIGHT_DIO = 1,
+      SWERVE_BACK_LEFT_DIO = 2,
+      SWERVE_BACK_RIGHT_DIO = 3,
+      INDEXER_CARGO_DIO = 4,
+      INTAKE_CARGO_DIO = 5,
+      HOOD_ENCODER_DIO = 6;
 
-  // Pneumatics Control Module (PCM) ports
-  public static final int COMPRESSOR_MODULE_ID = 0;
-  public static final int LEFT_INTAKE_SOLENOID_CHANNEL = 0;
-  public static final int RIGHT_INTAKE_SOLENOID_CHANNEL = 1;
+  /** Pneumatics Control Module (PCM) ports */
+  public static final int COMPRESSOR_MODULE_ID = 0,
+      LEFT_INTAKE_SOLENOID_CHANNEL = 0,
+      RIGHT_INTAKE_SOLENOID_CHANNEL = 1;
 }
