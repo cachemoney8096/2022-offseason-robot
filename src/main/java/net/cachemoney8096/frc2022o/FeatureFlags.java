346b3ef6ea6d37fd6f5e43dba306d4c2a66a008f
Basel Alghanem <basel@umich.edu>
2022-10-08 15:39:38 -0500

    Add buttons

--- src/main/java/net/cachemoney8096/frc2022o/FeatureFlags.java
+++ src/main/java/net/cachemoney8096/frc2022o/FeatureFlags.java
@@ -2,6 +2,6 @@ package net.cachemoney8096.frc2022o;
 
 /** Use this class to enable / disable certain special features */
 public class FeatureFlags {
-  /** If true, try to keep heading static when just translating while driving manually */
-  public static final boolean ENABLE_HEADING_LOCK_TELEOP = true;
+  /** TODO make this used */
+  public static final boolean DEFAULT_FIELD_RELATIVE = false;
 }
