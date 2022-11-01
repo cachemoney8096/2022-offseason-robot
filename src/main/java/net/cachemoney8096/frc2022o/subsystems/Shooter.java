5694e6c67857a3094476c07506ff19fdacb69574
tails618 <asher.grossman@gmail.com>
2022-11-01 17:37:08 -0500

    Should now be able to set shooter RPM and hood angle from dashboard

--- src/main/java/net/cachemoney8096/frc2022o/subsystems/Shooter.java
+++ src/main/java/net/cachemoney8096/frc2022o/subsystems/Shooter.java
@@ -177,7 +177,8 @@ public class Shooter extends SubsystemBase {
           return shooterSetpointRpm;
         },
         this::setShooterVelocity);
-    builder.addDoubleProperty("Shooter Velocity", this::getShooterVelocity, null);
+    builder.addDoubleProperty("Shooter Velocity", this::getShooterVelocity, this::setShooterVelocity);
+    builder.addDoubleProperty("Hood Angle", this::getHoodPositionDeg, this::setHoodPosition);
     builder.addBooleanProperty("Hood Position Ready", this::hoodPositionReady, null);
     builder.addBooleanProperty("Shooter Speed Ready", this::shooterSpeedReady, null);
     builder.addDoubleProperty("Hood kP", hoodController::getP, hoodController::setP);
